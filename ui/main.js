189e39f7d3a959733ef589731e0e21dc4918ec33
Loyel Robin Nazareth <nazloyel59@gmail.com>
2016-09-26 19:10:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,11 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 var img = document.getElementById('madi');
-img.onclick = function() {
-    img.style.marginLeft = '100px';
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
 }
+img.onclick = function() {
+    var interval = setInterval(moveRight, 100);
+};
