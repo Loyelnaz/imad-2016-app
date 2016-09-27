e26e6e8ec592a38d0d8ddd6e0b3b4db14abed4a7
Loyel Robin Nazareth <nazloyel59@gmail.com>
2016-09-27 20:08:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,13 +77,6 @@ app.get('/counter', function(req, res) {
   res.send(counter.toString());
 });
 
-var names = [];
-app.get('/submit-name/:name', function(req, res) {
-    var name = res.params.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
-
 app.get('/:articleName', function(req, res) {
     var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
@@ -101,6 +94,12 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var names = [];
+app.get('/submit-name/:name', function(req, res) {
+    var name = res.params.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
