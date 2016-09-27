3ec0bfeb1b9dc3f5e2ba8ae507fb4d0b73be7d12
Loyel Robin Nazareth <nazloyel59@gmail.com>
2016-09-27 13:57:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/counter', function(req, res) {
   res.send(counter.toString());
 });
 
-var names = '';
+var names = [];
 app.get('/submit-name', function(req, res) {
     var name = res.query.name;
     names.push(name);
