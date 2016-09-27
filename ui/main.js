d14c11cc3b2f71380484ddb628ee91f39951e32d
Loyel Robin Nazareth <nazloyel59@gmail.com>
2016-09-27 08:55:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,20 @@
-console.log('Loaded!');
+//Counter code 
 
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
+var button = document.getElementById('counter');
 
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight, 100);
+button.onclick = function() {
+    //Create a request
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    //Make a request
+    request.open('GET', 'http://loyelnaz.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
