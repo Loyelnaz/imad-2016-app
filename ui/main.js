650be54e717335b2596f5218052ab833fbff4b1d
Loyel Robin Nazareth <nazloyel59@gmail.com>
2016-09-27 20:33:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,8 +39,8 @@ submit.onclick = function() {
         }  
     };
     
-    var studentName = request.responseText;
-    var inputName = document.getElementById('name');
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://loyelnaz.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 }
