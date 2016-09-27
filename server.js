286b218422fa2c1737e35eda99ac77ecb5f080c8
Loyel Robin Nazareth <nazloyel59@gmail.com>
2016-09-27 20:09:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 var names = [];
 app.get('/submit-name/:name', function(req, res) {
-    var name = res.params.name;
+    var name = req.params.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
